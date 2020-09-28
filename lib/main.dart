4a10b71598d98e0d0fc4b45e579f2b4ba988b2d9
Ananthakrishna <ananthakrishna97@gmail.com>
2020-09-28 23:37:25 +0530

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_app/pages/choose_location.dart';
+import 'package:flutter_app/pages/home.dart';
+import 'package:flutter_app/pages/loading.dart';
+
+void main() {
+  runApp(MaterialApp(
+   initialRoute: '/home',
+    routes: {
+      '/' : (context) => Loading(),
+      '/home':(context) => Home(),
+      '/location': (context) =>ChooseLocation(),
+    },
+  ));
+}
